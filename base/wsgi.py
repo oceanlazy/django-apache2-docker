333d9bb046ee8f785e5d9c1a0f70fff0d4f21ab5
Vadim Kuznetsov <vadim.kuznyetsov@gmail.com>
2018-08-22 16:21:34 +0300

    startup code note

--- base/wsgi.py
+++ base/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 
+# some startup code, note: this block will be executed only after first visit on index page
+
 application = get_wsgi_application()
